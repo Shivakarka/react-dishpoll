cfdb07558ff6626fe6aab6c22528cb3de7b3eeb0
Shivakarka <shivakarka@gmail.com>
2022-06-13 19:29:52 +0530

    created components/routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Pollpage from "./components/Pollpage";
+import Results from "./components/Results";
+import PageNotFound from "./components/PageNotFound";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/pollpage" element={<Pollpage />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Pollpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pollpage() {
+  return (
+    <div>Pollpage</div>
+  )
+}
+
+export default Pollpage
